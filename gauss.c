00ee2929cf56ea3e60537a210c6a42ce4df84c52
gperouffe <g.perouffe@gmail.com>
2018-02-08 09:58:52 +0100

    Ajout permuter gauss

---
+++ gauss.c
@@ -0,0 +1,9 @@
+#include "mat.h"
+#include "vec.h"
+#include "gauss.h"
+
+void triangulation(T_Mat* pMat, T_Vec* pVec){}
+
+void remontee(T_Mat* pMat, T_Vec* pVec, T_Vec* pSolu){}
+
+void permuter(T_Mat* pMat, T_Vec* pVec, int p, int j){}
--- gauss.h
+++ gauss.h
@@ -5,4 +5,6 @@ void triangulation(T_Mat* pMat, T_Vec* pVec);
 
 void remontee(T_Mat* pMat, T_Vec* pVec, T_Vec* pSolu);
 
+void permuter(T_Mat* pMat, T_Vec* pVec, int p, int j);
+
 #endif
