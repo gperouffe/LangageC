973a76ff965ee896dd8b1af363becf0da9e3fdd1
gperouffe <g.perouffe@gmail.com>
2018-02-08 10:06:14 +0100

    Fonctions vec

--- vec.c
+++ vec.c
@@ -1,28 +1,40 @@
 #include "vec.h"
+#include <stdlib.h>
 
 /*
  * fonction vecAllouer
  * Description: Initialise la structure reperee par pVec
  *   pour qu'elle puisse contenir un vecteur de N éléments
  */
-void vecAllouer(T_Vec *pVec, int N){}
+void vecAllouer(T_Vec *pVec, int N){
+    pVec->N = N;
+    pVec->Elts = malloc(sizeof(double)*N);
+}
 
 /*
  * fonction vecAccElt
  * Description: retourne la valeur de  l'element en position i
  *    du vecteur pointe par pVec
  */
-double vecAccElt(T_Vec *pVec, int i){}
+double vecAccElt(T_Vec *pVec, int i){
+    return pVec->Elts[i];
+}
 
 /*
  * fonction vecModifElt
  * Description: Affecte Val a l'element en position i du 
  * vecteur repere par pVec
  */
-void vecModifElt(T_Vec *pVec, int i, double Val){}
+void vecModifElt(T_Vec *pVec, int i, double Val){
+    pVec->Elts[i] = Val;
+}
 
 /*
  * fonction : vecLiberer
  * Description : Affiche a l'ecran le contenu du vecteur repere par pVec
  */
-void vecLiberer(T_Vec *pVec){}
+void vecLiberer(T_Vec *pVec){
+    if(pVec->Elts!=NULL){
+        free(pVec->Elts);
+    }
+}
